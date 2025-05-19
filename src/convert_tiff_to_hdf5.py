401be50d73f638c0a81bd16df2d56fdfa1d8a485
KylesCorner <kylekrstulich@gmail.com>
2025-05-19 14:09:12 -0600

    started tiff to hdf5 file conversion

---
+++ src/convert_tiff_to_hdf5.py
@@ -0,0 +1,16 @@
+"""
+Author: Kyle Krstulich
+convert_tiff_to_hdf5.py
+
+"""
+import h5py
+import tifffile
+import utils
+
+
+def main():
+    print(utils.get_tiff_path())
+    return
+
+if __name__ == "__main__":
+    main()
