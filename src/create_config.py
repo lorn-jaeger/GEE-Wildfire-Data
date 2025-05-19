3f6d4eb6779fc1393a68f79b12ac5862d08e865e
KylesCorner <kylekrstulich@gmail.com>
2025-05-19 12:51:18 -0600

    config options fix

--- src/create_config.py
+++ src/create_config.py
@@ -10,10 +10,10 @@ def create_fire_config_globfire(geojson_path, output_path, year):
     # print(f"[LOG] from create_config, gdf: {gdf}")
     gdf['IDate'] = pd.to_datetime(gdf['IDate'], unit='ms')
     gdf['FDate'] = pd.to_datetime(gdf['FDate'], format='mixed')
-    print(f"[LOG] from create_config, gdf Idate: {gdf['IDate']}")
+    # print(f"[LOG] from create_config, gdf Idate: {gdf['IDate']}")
     
     gdf = gdf[gdf['IDate'].dt.year == int(year)]
-    print(f"[LOG] from create_config, second gdf: {gdf}")
+    # print(f"[LOG] from create_config, second gdf: {gdf}")
     first_occurrences = gdf.sort_values('IDate').groupby('Id').first()
     last_occurrences = gdf.sort_values('IDate').groupby('Id').last()
     
