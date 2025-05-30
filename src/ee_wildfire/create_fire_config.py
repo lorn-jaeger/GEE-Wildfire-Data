e2dba49ba238a769d260bf87cde801376f712fc8
KylesCorner <kylekrstulich@gmail.com>
2025-05-30 03:30:24 -0600

    removed print statements

--- src/ee_wildfire/create_fire_config.py
+++ src/ee_wildfire/create_fire_config.py
@@ -8,21 +8,13 @@ from ee_wildfire.utils.yaml_utils import get_full_yaml_path
 
 def create_fire_config_globfire(config):
     output_path = get_full_yaml_path(config)
-    print(output_path)
     year = config.start_date.year
-    # geojson_path = get_full_geojson_path(config)
-    # FIX: not saving geojson, just pull value from get_globfire.py
 
-    # print(f"[LOG] from create_config, geojson_path: {geojson_path}")
     gdf = config.geodataframe
-    # gdf = gpd.read_file(geojson_path)
-    # print(f"[LOG] from create_config, gdf: {gdf}")
     gdf['IDate'] = pd.to_datetime(gdf['IDate'], unit='ms')
     gdf['FDate'] = pd.to_datetime(gdf['FDate'], format='mixed')
-    # print(f"[LOG] from create_config, gdf Idate: {gdf['IDate']}")
 
     gdf = gdf[gdf['IDate'].dt.year == int(year)]
-    # print(f"[LOG] from create_config, second gdf: {gdf}")
     first_occurrences = gdf.sort_values('IDate').groupby('Id').first()
     last_occurrences = gdf.sort_values('IDate').groupby('Id').last()
 
