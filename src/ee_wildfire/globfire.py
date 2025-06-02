1cdf7e3f123bb5068b817fc0b43d1eb4a704795c
Lorn Jaeger <lornjaeger@proton.me>
2025-06-02 09:06:03 -0600

    removed yaml dumper

--- src/ee_wildfire/globfire.py
+++ src/ee_wildfire/globfire.py
@@ -127,54 +127,6 @@ def get_fires(config):
     }).reset_index()   
 
     return gdf
-
-
-def create_fire_config_globfire(user_config) -> None:
-    output_path = get_full_yaml_path(user_config)
-    year = user_config.start_date.year
-
-    gdf = user_config.geodataframe
-    gdf['IDate'] = pd.to_datetime(gdf['IDate'], unit='ms')
-    gdf['FDate'] = pd.to_datetime(gdf['FDate'], format='mixed')
-
-    gdf = gdf[gdf['IDate'].dt.year == int(year)]
-    first_occurrences = gdf.sort_values('IDate').groupby('Id').first() #type: ignore
-    last_occurrences = gdf.sort_values('IDate').groupby('Id').last() #type: ignore
-
-    config = {
-        'output_bucket': 'firespreadprediction',
-        'rectangular_size': 0.5, 'year': year }
-
-    # ensures that datetime objects are dumped as YYYY-MM-DD
-    class DateSafeYAMLDumper(yaml.SafeDumper):
-        def represent_data(self, data):
-            if isinstance(data, datetime):
-                return self.represent_scalar('tag:yaml.org,2002:timestamp', data.strftime('%Y-%m-%d'))
-            return super().represent_data(data)
-
-    # Populate fire entries
-    for idx in first_occurrences.index:
-        first = first_occurrences.loc[idx]
-        last = last_occurrences.loc[idx]
-
-        # no final date, just pulls last inital date
-        end_date = last['FDate'] if pd.notna(last['FDate']) else last['IDate']
-        # 4 day buffer before and after ignition/containment
-        start_date = first['IDate'] - timedelta(days=4)
-        end_date = end_date + timedelta(days=4)
-
-        config[f'fire_{idx}'] = {
-            'latitude': float(first['lat']),
-            'longitude': float(first['lon']),
-            'start': start_date.date(),
-            'end': end_date.date()
-        }
-    # Ensure the directory exists
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-    with open(output_path, 'w') as f:
-        yaml.dump(config, f, Dumper=DateSafeYAMLDumper, default_flow_style=False, sort_keys=False)
-   
                 
 
 def main():
