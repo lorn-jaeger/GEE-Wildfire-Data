82f87b57a019a23b4147d49c52e69777f560887b
KylesCorner <kylekrstulich@gmail.com>
2025-06-06 16:13:11 -0600

    show config fix

--- src/ee_wildfire/command_line_args.py
+++ src/ee_wildfire/command_line_args.py
@@ -92,13 +92,13 @@ def parse() -> UserConfig:
 
     outside_user_config_path = args.config
 
+    ConsoleUI.set_verbose(args.verbose)
     config = UserConfig(yaml_path=outside_user_config_path)
     config.change_configuration_from_args(args)
 
     if(args.show_config or (args.config != INTERNAL_USER_CONFIG_DIR)):
-        ConsoleUI.print(str(config))
+        print(str(config))
 
-    ConsoleUI.set_verbose(args.verbose)
 
 
     return config
