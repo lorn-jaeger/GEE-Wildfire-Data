cdb6960fe8766d19510adf286e4bcf80357e0dc0
KylesCorner <kylekrstulich@gmail.com>
2025-06-23 11:26:30 -0600

    test edit

--- test/test_user_config.py
+++ test/test_user_config.py
@@ -1,17 +1,19 @@
-from unittest.mock import patch
 from datetime import datetime
+from pathlib import Path
+from unittest.mock import patch
+
 from ee_wildfire.command_line_args import parse
 from ee_wildfire.constants import *
-from pathlib import Path
-
 
 SERVICE_ACCOUNT = Path("/home/kyle/NRML/OAuth/service-account-credentials.json")
 DATA_DIR = Path("/home/kyle/opt")
-TIFF_DIR = Path("/home/kyle/opt/google-cloud-sdk")
+TIFF_DIR = Path("/home/kyle/opt/tiff_test")
+LOG_DIR = DATA_DIR / "log_test"
+LOG_LVL = "debug"
 DRIVE_DIR = "test_dir"
 
-# TODO: update this test
 
+# TODO: update this test
 
 
 def __load_config(args_dict):
@@ -22,58 +24,82 @@ def __load_config(args_dict):
                 cli_args.append(key)
         else:
             cli_args.extend([key, str(value)])
-    test_args = ['prog'] + cli_args
+    test_args = ["prog"] + cli_args
 
-    with patch('sys.argv', test_args):
+    with patch("sys.argv", test_args):
         config = parse()
     return config
 
+
 def test_reset():
     args_dict = {
-        '--silent': None,
-        '--reset-config': True,
-        '--credentials':SERVICE_ACCOUNT,
+        "--silent": None,
+        "--reset-config": True,
+        "--credentials": SERVICE_ACCOUNT,
     }
     config = __load_config(args_dict)
 
     assert config.silent, "Expected `config.silent` to be True"
-    assert config.reset_config,  "Expected `config.reset_config` to be True"
+    assert config.reset_config, "Expected `config.reset_config` to be True"
     assert not config.export, "Expected `config.export` to be False"
     assert not config.download, "Expected `config.download` to be False"
     assert not config.retry_failed, "Expected `config.retry_failed` to be False"
     assert not config.purge_after, "Expected `config.purge_after` to be False"
     assert not config.purge_before, "Expected `config.purge_before` to be False"
 
-    assert config.tiff_dir == DEFAULT_TIFF_DIR, f"Expected `config.tiff_dir` to be {DEFAULT_TIFF_DIR}"
-    assert config.data_dir == DEFAULT_DATA_DIR, f"Expected `config.data_dir` to be {DEFAULT_DATA_DIR}"
-    assert config.start_date == DEFAULT_START_DATE, f"Expected `config.start_date` to be {DEFAULT_START_DATE}"
-    assert config.end_date == DEFAULT_END_DATE, f"Expected `config.end_date` to be {DEFAULT_END_DATE}"
-    assert config.credentials == SERVICE_ACCOUNT, f"Expected `config.credentials` to be {SERVICE_ACCOUNT}"
-    assert config.google_drive_dir == DEFAULT_GOOGLE_DRIVE_DIR, f"Expected `config.google_drive_dir` to be {DEFAULT_GOOGLE_DRIVE_DIR}"
-    assert config.min_size == DEFAULT_MIN_SIZE, f"Expected `config.min_size` to be {DEFAULT_MIN_SIZE}"
-    assert config.max_size == DEFAULT_MAX_SIZE, f"Expected `config.max_size` to be {DEFAULT_MAX_SIZE}"
+    assert (
+        config.log_dir == DEFAULT_LOG_DIR
+    ), f"Exptected `config.log_dir` to be {DEFAULT_LOG_DIR}"
+    assert (
+        config.log_level == DEFAULT_LOG_LEVEL
+    ), f"Expected `config.log_level` to be {DEFAULT_LOG_LEVEL}"
 
+    assert (
+        config.tiff_dir == DEFAULT_TIFF_DIR
+    ), f"Expected `config.tiff_dir` to be {DEFAULT_TIFF_DIR}"
+    assert (
+        config.data_dir == DEFAULT_DATA_DIR
+    ), f"Expected `config.data_dir` to be {DEFAULT_DATA_DIR}"
+    assert (
+        config.start_date == DEFAULT_START_DATE
+    ), f"Expected `config.start_date` to be {DEFAULT_START_DATE}"
+    assert (
+        config.end_date == DEFAULT_END_DATE
+    ), f"Expected `config.end_date` to be {DEFAULT_END_DATE}"
+    assert (
+        config.credentials == SERVICE_ACCOUNT
+    ), f"Expected `config.credentials` to be {SERVICE_ACCOUNT}"
+    assert (
+        config.google_drive_dir == DEFAULT_GOOGLE_DRIVE_DIR
+    ), f"Expected `config.google_drive_dir` to be {DEFAULT_GOOGLE_DRIVE_DIR}"
+    assert (
+        config.min_size == DEFAULT_MIN_SIZE
+    ), f"Expected `config.min_size` to be {DEFAULT_MIN_SIZE}"
+    assert (
+        config.max_size == DEFAULT_MAX_SIZE
+    ), f"Expected `config.max_size` to be {DEFAULT_MAX_SIZE}"
 
 
 def test_dict_style_arguments():
     args_dict = {
-        '--export': True,
-        '--download': True,
-        '--retry-failed': True,
-        '--data-dir': DATA_DIR,
-        '--tiff-dir': TIFF_DIR,
-        '--start-date': '2021-01-01',
-        '--end-date': '2021-01-14',
-        '--silent': None,
-        '--credentials': SERVICE_ACCOUNT,
-        '--google-drive-dir': DRIVE_DIR,
-        '--purge-after': True,
-        '--purge-before': True,
-        '--min-size': 10,
-        '--max-size': 100,
+        "--export": True,
+        "--download": True,
+        "--retry-failed": True,
+        "--data-dir": DATA_DIR,
+        "--tiff-dir": TIFF_DIR,
+        "--start-date": "2021-01-01",
+        "--end-date": "2021-01-14",
+        "--silent": None,
+        "--credentials": SERVICE_ACCOUNT,
+        "--google-drive-dir": DRIVE_DIR,
+        "--purge-after": True,
+        "--purge-before": True,
+        "--min-size": 10,
+        "--max-size": 100,
+        "--log-dir": LOG_DIR,
+        "--log-level": LOG_LVL,
     }
     config = __load_config(args_dict)
-
 
     assert config.silent, "Expected `config.silent` to be True"
     assert config.purge_after, "Expected `config.purge_after` to be True"
@@ -82,14 +108,26 @@ def test_dict_style_arguments():
     assert config.download, "Expected `config.download` to be True"
     assert config.retry_failed, "Expected `config.retry_failed` to be True"
 
+    assert config.log_dir == LOG_DIR, f"Exptected `config.log_dir` to be {LOG_DIR}"
+    assert config.log_level == LOG_LVL, f"Expected `config.log_level` to be {LOG_LVL}"
+
     assert config.tiff_dir == TIFF_DIR, f"Expected `config.tiff_dir` to be {TIFF_DIR}"
     assert config.data_dir == DATA_DIR, f"Expected `config.data_dir` to be {DATA_DIR}"
-    assert config.start_date == datetime(2021, 1, 1), "Expected `config.start_date` to be 2021-01-01"
-    assert config.end_date == datetime(2021, 1, 14), "Expected `config.end_date` to be 2021-01-14"
-    assert config.credentials == SERVICE_ACCOUNT, f"Expected `config.credentials` to be {SERVICE_ACCOUNT}"
-    assert config.google_drive_dir == DRIVE_DIR, f"Expected `config.google_drive_dir` to be {DRIVE_DIR}"
+    assert config.start_date == datetime(
+        2021, 1, 1
+    ), "Expected `config.start_date` to be 2021-01-01"
+    assert config.end_date == datetime(
+        2021, 1, 14
+    ), "Expected `config.end_date` to be 2021-01-14"
+    assert (
+        config.credentials == SERVICE_ACCOUNT
+    ), f"Expected `config.credentials` to be {SERVICE_ACCOUNT}"
+    assert (
+        config.google_drive_dir == DRIVE_DIR
+    ), f"Expected `config.google_drive_dir` to be {DRIVE_DIR}"
     assert config.min_size == 10, "Expected `config.min_size` to be 10"
     assert config.max_size == 100, "Expected `config.max_size` to be 100"
+
 
 def test_internal_config_after_arg_change():
     args_dict = {
@@ -104,13 +142,22 @@ def test_internal_config_after_arg_change():
     assert not config.purge_after, "Expected `config.purge_after` to be False"
     assert not config.purge_before, "Expected `config.purge_before` to be False"
 
+    assert config.log_dir == LOG_DIR, f"Exptected `config.log_dir` to be {LOG_DIR}"
+    assert config.log_level == LOG_LVL, f"Expected `config.log_level` to be {LOG_LVL}"
+
     assert config.tiff_dir == TIFF_DIR, f"Expected `config.tiff_dir` to be {TIFF_DIR}"
     assert config.data_dir == DATA_DIR, f"Expected `config.data_dir` to be {DATA_DIR}"
-    assert config.start_date == datetime(2021, 1, 1), "Expected `config.start_date` to be 2021-01-01"
-    assert config.end_date == datetime(2021, 1, 14), "Expected `config.end_date` to be 2021-01-14"
-    assert config.credentials == SERVICE_ACCOUNT, f"Expected `config.credentials` to be {SERVICE_ACCOUNT}"
-    assert config.google_drive_dir == DRIVE_DIR, f"Expected `config.google_drive_dir` to be {DRIVE_DIR}"
+    assert config.start_date == datetime(
+        2021, 1, 1
+    ), "Expected `config.start_date` to be 2021-01-01"
+    assert config.end_date == datetime(
+        2021, 1, 14
+    ), "Expected `config.end_date` to be 2021-01-14"
+    assert (
+        config.credentials == SERVICE_ACCOUNT
+    ), f"Expected `config.credentials` to be {SERVICE_ACCOUNT}"
+    assert (
+        config.google_drive_dir == DRIVE_DIR
+    ), f"Expected `config.google_drive_dir` to be {DRIVE_DIR}"
     assert config.min_size == 10, "Expected `config.min_size` to be 10"
     assert config.max_size == 100, "Expected `config.max_size` to be 100"
-
-
