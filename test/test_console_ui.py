5d882f155ee41caec70a4b526b9d2898ee556f8b
KylesCorner <kylekrstulich@gmail.com>
2025-08-14 14:10:19 -0600

    added more unit tests

--- test/test_console_ui.py
+++ test/test_console_ui.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 from ee_wildfire.UserInterface.UserInterface import ConsoleUI
 
-DATA_DIR = Path("/tmp/ee_wildfire_logs/").mkdir(parents=True, exist_ok=True)
-LOG_DIR = Path(DATA_DIR / "log_test").mkdir(parents=True, exist_ok=True)
+DATA_DIR = Path("/tmp/ee_wildfire_logs/")
+LOG_DIR = DATA_DIR / "log_test"
 
 
 def test_multithreaded_logging(tmp_path):
